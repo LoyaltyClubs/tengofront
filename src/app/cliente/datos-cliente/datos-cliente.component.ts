da9cde29fec2f402df485f349379b3b9235a31df
danae <danna.android@gmail.com>
2021-09-09 20:59:08 -0400

    cambios en cliente

--- src/app/cliente/datos-cliente/datos-cliente.component.ts
+++ src/app/cliente/datos-cliente/datos-cliente.component.ts
@@ -26,7 +26,40 @@ export class DatosClienteComponent implements OnInit {
 
   ngOnInit(): void {
     this.formGroup = <FormGroup>this.controlContainer.control;
+    this.setFormValidations()
     this.disableControls();
+  }
+
+  setFormValidations() {
+    this.formGroup.get('nombre')?.setValidators([Validators.required, Validators.minLength(3),])
+    this.formGroup.get('apellido_paterno')?.setValidators([Validators.required])
+    this.formGroup.get('apellido_materno')?.setValidators([Validators.required])
+    this.formGroup.get('estado_civil')?.setValidators([Validators.required])
+    this.formGroup.get('fecha_nacimiento')?.setValidators([Validators.required])
+    this.formGroup.get('sexo')?.setValidators([Validators.required])
+    this.formGroup.get('ci')?.setValidators([Validators.required])
+    this.formGroup.get('calle_particular')?.setValidators([Validators.required])
+    this.formGroup.get('zona')?.setValidators([Validators.required])
+    this.formGroup.get('provincia')?.setValidators([Validators.required])
+    this.formGroup.get('barrio')?.setValidators([Validators.required])
+    this.formGroup.get('ciudad_id')?.setValidators([Validators.required])
+    this.formGroup.get('telefono_fijo')?.setValidators([Validators.minLength(7), Validators.pattern('[1-9]{1}\\d+'),])
+    this.formGroup.get('telefono_celular')?.setValidators([Validators.required,
+      Validators.minLength(7),
+      Validators.pattern('[1-9]{1}\\d+'),])
+    this.formGroup.get('email')?.setValidators([Validators.required])
+    this.formGroup.get('parentesco_referencia')?.setValidators([Validators.required])
+    this.formGroup.get('nombre_referencia')?.setValidators([Validators.required])
+    this.formGroup.get('provincia_referencia')?.setValidators([Validators.required])
+    this.formGroup.get('telefono_referencia')?.setValidators([Validators.required])
+    this.formGroup.get('tipo_tel_referencia')?.setValidators([Validators.required])
+    this.formGroup.get('ciudad_referencia')?.setValidators([Validators.required])
+    this.formGroup.get('dia_pago')?.setValidators([Validators.required])
+    this.formGroup.get('estado')?.setValidators([Validators.required])
+    this.formGroup.get('saldo')?.setValidators([Validators.required])
+
+    this.formGroup.markAllAsTouched()
+    this.formGroup.updateValueAndValidity()
   }
 
   editarCliente() {
@@ -34,6 +67,12 @@ export class DatosClienteComponent implements OnInit {
   }
 
   actualizarCliente() {
+    console.log(this.formGroup.invalid)
+
+    if (this.formGroup.invalid) {
+      console.log('invalid')
+      return
+    }
     this.clienteService.edit(this.formGroup.value).subscribe(
       (res: Cliente) => {
         this.formGroup = this.formBuilder.group(res);
@@ -55,5 +94,10 @@ export class DatosClienteComponent implements OnInit {
   enableControls() {
     this.textEditBtn = 'Guardar';
     this.editing = true
+  }
+
+  isInvalid(inputName: string): boolean {
+    const control = this.formGroup.controls[inputName]
+    return control.errors != null && control.touched
   }
 }
