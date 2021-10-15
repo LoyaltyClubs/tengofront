5985938a237541727bc72cd1467107b67e936f41
danae <danna.android@gmail.com>
2021-10-14 22:44:04 -0400

    SISLO-445

--- src/app/empresas/empresas/empresas-editar/empresas-editar.component.ts
+++ src/app/empresas/empresas/empresas-editar/empresas-editar.component.ts
@@ -1,20 +1,30 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { EmpresaCiudad, Plan, Rubro } from 'src/app/api-client/api.types';
+import { EmpresaCiudadService } from 'src/app/api-client/empresa-ciudad.service';
+import { RubroService } from 'src/app/api-client/rubro.service';
 import { EmpresaService } from 'src/app/services/empresa.service';
+import { PlanService } from 'src/app/services/plan.service';
 
 @Component({
   selector: 'app-empresas-editar',
   templateUrl: './empresas-editar.component.html'
 })
 export class EmpresasEditarComponent {
+
+  listPlanes: Plan[] = [];
+  rubros: Rubro[] = [];
+  ciudades: EmpresaCiudad[] = [];
+
   formGroup: FormGroup = this.formBuilder.group({
       nombre: ['', Validators.required],
       razon_social: ['', Validators.required],
       nit: ['', Validators.required],
       direccion: ['', Validators.required],
       fecha_cierre: ['', Validators.required],
-      inicio_contrato: ['', Validators.required],
+      inicio_contrato: [formatDate(new Date(), 'yyyy-MM-dd', 'en'), Validators.required],
       fin_contrato: ['', Validators.required],
       representante_legal: ['', Validators.required],
       email: ['', Validators.required],
@@ -32,11 +42,20 @@ export class EmpresasEditarComponent {
     private formBuilder: FormBuilder,
     private router: Router,
     private empresaService: EmpresaService,
-    private activatedroute: ActivatedRoute
+    private activatedroute: ActivatedRoute,
+    readonly ciudadService: EmpresaCiudadService,
+    private _planService: PlanService,
+    readonly rubroService: RubroService
   ) {
     this.activatedroute.queryParams.subscribe((data) => {
       this.formGroup = this.formBuilder.group(data)
     });
+  }
+
+  ngOnInit(): void {
+    this.obtenerCiudades()
+    this.obtenerPlanes()
+    this.obtenerRubros()
   }
 
   editarEmpresa() {
@@ -49,6 +68,41 @@ export class EmpresasEditarComponent {
         console.log(error);
         this.formGroup.reset();
       }
+    );
+  }
+
+  obtenerPlanes() {
+    this._planService.getPlan().subscribe(
+      (data) => {
+        this.listPlanes = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  obtenerCiudades() {
+    this.ciudadService.getAll().subscribe(
+      (res: EmpresaCiudad[]) => {
+        this.ciudades = res;
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {}
+    );
+  }
+
+  obtenerRubros() {
+    this.rubroService.getAll().subscribe(
+      (res: Rubro[]) => {
+        this.rubros = res;
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {}
     );
   }
 }
